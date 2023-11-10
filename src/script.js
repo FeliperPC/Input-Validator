770af5a912eaf5bd4036b80d5ada21a379294b05
Felipe Padovani <padovani1010@gmail.com>
2023-11-09 22:28:08 -0300

    Exercicio finalizado

--- src/script.js
+++ src/script.js
@@ -1,1 +1,18 @@
 import './style.css';
+import validator from 'validator';
+
+const input = document.querySelector('input');
+const btn = document.querySelector('button');
+const selection = document.querySelector('select');
+const result = document.querySelector('p');
+
+btn.addEventListener('click', () => {
+  const info = {
+    cpf: validator.isTaxID(input.value, 'pt-BR'),
+    phoneNumber: validator.isMobilePhone(input.value),
+    email: validator.isEmail(input.value),
+    url: validator.isURL(input.value),
+  };
+
+  result.innerHTML = `the validation returned ${info[selection.value]}`;
+});
